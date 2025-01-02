d2b9f50b6164bcc0f460f9ed16df5c2561a4f539
GeorgeC137 <omollogeorge17@gmail.com>
2025-01-02 14:58:11 +0300

    working on components

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+
+      {/* <!-- Hero --> */}
+      <Hero />
+
+      {/* <!-- Developers and Employers --> */}
+      <HomeCards />
+
+      {/* <!-- Browse Jobs --> */}
+      <JobListings />
+
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default App;
